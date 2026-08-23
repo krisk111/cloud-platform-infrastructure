923c820399c03bdb692280c6166c2191dc2b69b0
Krishna Kothari <krishnarzk@gmail.com>
2026-08-23 13:41:12 -0500

    chore(infrastructure): increase EKS node group capacity

--- terraform/eks-node-group.tf
+++ terraform/eks-node-group.tf
@@ -11,7 +11,7 @@ resource "aws_eks_node_group" "cloud_platform_eks_node_group" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
     max_size     = 2
   }
